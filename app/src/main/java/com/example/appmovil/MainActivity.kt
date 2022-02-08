5b160d141c0a72a803892103cd6741c03ddb34ac
Robinhenao <65988533+Robinhenao@users.noreply.github.com>
2022-02-07 21:40:21 -0500

    v 1.1

--- app/src/main/java/com/example/appmovil/MainActivity.kt
+++ app/src/main/java/com/example/appmovil/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.appmovil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    var boton : Button?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boton = findViewById(R.id.Button_ingreso)
+
+        boton!!.setOnClickListener{
+            setContentView(R.layout.activity_ingreso)
+        }
+
     }
+
+
 }
