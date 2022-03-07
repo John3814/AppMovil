d8b4119e351fbcc80fb6f05ab68b78a0c5dbe809
Robin Henao <robinson.henao@udea.edu.co>
2022-03-07 16:22:11 -0500

    add sql order list

--- app/src/main/java/com/example/appmovil/ActivityBalance.kt
+++ app/src/main/java/com/example/appmovil/ActivityBalance.kt
@@ -1,17 +1,20 @@
 package com.example.appmovil
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.TableLayout
 import android.widget.TextView
 
-class ActivityBalance : AppCompatActivity() {
-    var tlBalance:TableLayout?=null
-    var spinnerListBy=0
-    var spinnerBySort=0
+class ActivityBalance : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private var tlBalance:TableLayout?=null
+    private var spinnerListBy = 0
+    private var spinnerBySort = 0
+    private val con=SQLite(this,"basemovil",null,1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,35 +22,52 @@ class ActivityBalance : AppCompatActivity() {
 
         tlBalance=findViewById(R.id.tlBalance)
 
-        spinnerListBy=findViewById<Spinner>(R.id.spinnerList).selectedItemPosition
-        spinnerBySort=findViewById<Spinner>(R.id.spinnerSort).selectedItemPosition
-
-        llenarTabla()
+        findViewById<Spinner>(R.id.spinnerList).onItemSelectedListener=this
+        findViewById<Spinner>(R.id.spinnerSort).onItemSelectedListener=this
 
     }
-    private fun selectSpinnerTable(): String {
-        return when (spinnerListBy) {
+    private fun selectSpinnerTable() {
+        spinnerListBy=findViewById<Spinner>(R.id.spinnerList).selectedItemPosition
+
+        when (spinnerListBy) {
             0 -> {
-                "Ingresos"
+                consult("Ingresos")
             }
             1 -> {
-                "Egresos"
+                consult("Egresos")
             }
             2 -> {
-                "CuentaxPagar"
+                consult("CuentaxPagar")
             }
             else -> {
-                "CuentaxCobrar"
+                consult("CuentaxCobrar")
             }
+        }
+    }
+    private fun consult(schem:String){
+        spinnerBySort=findViewById<Spinner>(R.id.spinnerSort).selectedItemPosition
+        val fila:Cursor
+        val baseDatos=con.writableDatabase
+        when(spinnerBySort){
+            0 -> {
+                fila=baseDatos.rawQuery("select id,date,concept,value from $schem" ,null)
+                llenarTabla(fila)
+            }
+            1 -> {
+                fila=baseDatos.rawQuery("select id,date,concept,value from $schem where date" ,null)
+                llenarTabla(fila)
+            }
+            2 -> {
+                fila=baseDatos.rawQuery("select id,date,concept,value from $schem where value ORDER BY value desc" ,null)
+                llenarTabla(fila)
+            }
+
         }
     }
 
 
-    fun llenarTabla(){
-        val table= selectSpinnerTable()
-        val con=SQLite(this,"basemovil",null,1)
-        val baseDatos=con.writableDatabase
-        val fila=baseDatos.rawQuery("select id,date,concept,value from $table",null)
+    private fun llenarTabla(fila:Cursor){
+        tlBalance?.removeAllViews()
         fila.moveToFirst()
         do {
             val registro=LayoutInflater.from(this).inflate(R.layout.item_table,null,false)
@@ -60,9 +80,14 @@ class ActivityBalance : AppCompatActivity() {
             tvConcept.setText(fila.getString(2))
             tvValue.setText(fila.getString(3))
             tlBalance?.addView(registro)
-
-
         }while (fila.moveToNext())
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        selectSpinnerTable()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
 
     }
 }
